2ff217f5fa98e31e9b7928799e015924f3e97d56
Maiquel <junior.mafra13062005@gmail.com>
2026-04-20 15:55:18 -0300

    Estudado Crud!

--- Teste/obj/Debug/net10.0/Teste.AssemblyInfo.cs
+++ Teste/obj/Debug/net10.0/Teste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+987eb93a5f6def3a3ccac630f858a7b928836c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c32f90dbe62584de1a61e636945d4f58da0d03b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
