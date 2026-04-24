742ad2e6014c71c377e564ac5a5999b6fbb7a4bc
Maiquel <junior.mafra13062005@gmail.com>
2026-04-24 17:18:34 -0300

    ..

---
+++ atividadeapi/src/test/java/com/example/atividadeapi/AtividadeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
