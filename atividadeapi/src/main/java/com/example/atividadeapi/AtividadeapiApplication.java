a24cc455ca99363b1595d868fca1635b8463b2dc
Maiquel <junior.mafra13062005@gmail.com>
2026-04-24 17:17:11 -0300

    Arquitetura

---
+++ atividadeapi/src/main/java/com/example/atividadeapi/AtividadeapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividadeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeapiApplication.class, args);
+	}
+
+}
